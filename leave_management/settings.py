2a8a3eacb8c2c7245850b4c31dee80d97556db93
Sakshi-patil2004 <sakshipatil152004@gmail.com>
2025-04-27 17:02:39 +0530

    Update settings.py

--- leave_management/settings.py
+++ leave_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-47n^xl@km5z9__of7t6ox6+olct9q@y+p1jx#jk*=+vsnn!pp2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
